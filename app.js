daa324512f161bfd33069b2509ea7da8a53223a0
Kevin Kim <kevinkim75@gmail.com>
2020-12-23 13:43:34 -0500

    Completed the app

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const base = require('airtable').base('app5LznoARFjt0MpW');
+
+const app = express();
+const dir = path.join(__dirname, 'img');
+app.use(express.static(dir));
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, '.'));
+
+let records;
+app.get('/', async (req, res) => {
+  if (records) {
+    res.render('page', { records });
+  } else {
+    records = await base('Business Hours')
+      .select({ view: 'Grid view' })
+      .firstPage();
+    res.render('page', { records });
+    setTimeout(() => {
+      records = null;
+    }, 10 * 1000);
+  }
+});
+
+app.listen(3000, () => console.log('Server ready'));
